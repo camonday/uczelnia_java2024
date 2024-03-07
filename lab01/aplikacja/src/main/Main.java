a439c29b08305c77f7818e9273a62c6f9882873d
arkad <arkadiakowalczyk@gmail.com>
2024-03-07 13:28:40 +0100

    ??

--- lab01/aplikacja/src/main/Main.java
+++ lab01/aplikacja/src/main/Main.java
@@ -1,23 +1,26 @@
 package main;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 import obliczeniaP.Zipper;
+import obliczeniaP.Checker;
 
 
 public class Main {
 
     static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         //System.out.println("hello");
 
         String filesPath = "";
         String newPath = "";
 
-        // todo zakoduj sciezke gdzie beda wyniki
+        // done zakoduj sciezke gdzie beda wyniki
         String path_for_zipPairs = "/zipPairs";
 
-        // todo pokaz menu
+        // done pokaz menu
         switch(useMenu()){
             case 1:
                 // odbierz sciezke tego co masz zapakowac
@@ -31,33 +34,21 @@ public class Main {
                     System.out.println(e.getMessage());
 
                 }
-
-
-                //zapisz zip
-
                 break;
             case 2:
 
-                System.out.print("Sciezka do zipa ktory mam sprawdzic: ");
+                System.out.print("Sciezka do folderu z zipem ktory mam sprawdzic: ");
                 filesPath = sc.next();
+
+                if(Checker.checkMD5(filesPath)){
+                    System.out.println("Klucze sie zgadzają");
+                }
+                else{
+                    System.out.println("Klucze się nie zgadają - plik został naruszony");
+                }
 
                 break;
         }
-
-
-
-        // todo zrob zip
-        //filesPath = sc.next();
-
-
-        // todo zapisz zip
-
-        // todo policz md5 dla zip
-
-        // todo zapisz md5
-
-        //---
-        // todo porownaj obliczone md5 z odczytanym md5
 
         return;
     }
--- lab01/obliczenia/src/obliczeniaP/Checker.java
+++ lab01/obliczenia/src/obliczeniaP/Checker.java
@@ -1,4 +1,43 @@
 package obliczeniaP;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+
 public class Checker {
+    public static boolean checkMD5(String folderPath) throws IOException, NoSuchAlgorithmException {
+        String filePath = createZipFilePath(folderPath);
+        String oldie = readFileContent(folderPath);
+        String newbie = CounterMD5.countMD5(filePath);
+
+        return oldie.equals(newbie);
+    }
+
+    public static String readFileContent(String folderPath){
+        Path filePath = Paths.get(folderPath, "MD5.txt");
+        String content = "";
+
+        try {
+            content = new String(Files.readAllBytes(filePath));
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
+
+        return content;
+    }
+
+    public static String createZipFilePath(String folderPath) {
+        Path path = Paths.get(folderPath);
+        Path fileName = path.getFileName(); // Get the last part of the path
+        if (fileName != null) {
+            String zipFileName = fileName.toString() + ".zip"; // Append .zip to the last part of the path
+            folderPath += "/" +zipFileName;
+            return folderPath;
+        } else {
+            return null;
+        }
+    }
 }
